3d2985be1a07db28bb8468088efb61ed2d6a5941
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-08-22 02:35:41 -0700

    samll skills

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -15,44 +15,44 @@ export default function Skills({isDarkMode, setIsDarkMode}) {
 
         <div className="row">
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center">
-            <img src="../assets/Skill1.svg" alt="" />
+            <img src="../assets/skill1.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center">
-            <img src="../assets/Skill2.svg" alt="" />
+            <img src="../assets/skill2.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center res-mt-575">
-            <img src="../assets/Skill3.svg" alt="" />
+            <img src="../assets/skill3.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center res-mt-992">
-            <img src="../assets/Skill4.svg" alt="" />
+            <img src="../assets/skill4.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center res-mt-992">
-            <img src="../assets/Skill5.png" alt="" />
+            <img src="../assets/skill5.png" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center res-mt-992">
-            <img src="../assets/Skill6.svg" alt="" />
+            <img src="../assets/skill6.svg" alt="" />
           </div>
         </div>
 
         <div className="row mt-skill-row">
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center">
-            <img src="../assets/Skill7.svg" alt="" />
+            <img src="../assets/skill7.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center">
-            <img src="../assets/Skill8.svg" alt="" />
+            <img src="../assets/skill8.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center res-mt-575">
-            <img src="../assets/Skill9.svg" alt="" />
+            <img src="../assets/skill9.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center  res-mt-992">
           { isDarkMode ? <img src="../assets/skill10dark.png" alt="" /> :    <img src="../assets/Skill10.svg" alt="" />}
           
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center  res-mt-992">
-            <img src="../assets/Skill11.svg" alt="" />
+            <img src="../assets/skill11.svg" alt="" />
           </div>
           <div className="col-lg-2 col-md-4 col-sm-4 col-6 d-flex justify-content-center  res-mt-992">
-          { isDarkMode ? <img src="../assets/skill12dark.png" alt="" /> :  <img src="../assets/Skill12.svg" alt="" />}
+          { isDarkMode ? <img src="../assets/skill12dark.png" alt="" /> :  <img src="../assets/skill12.svg" alt="" />}
             
           </div>
         </div>
